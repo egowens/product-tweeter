31c45451d6917e3f36441cde9a88f9a9ff5bb224
Eric Gowens <eric.gowens@gmail.com>
2014-10-10 09:53:20 +0200

    updated inmemorystoreage to be in Shop

--- config/initializers/shopify_session_repository.rb
+++ config/initializers/shopify_session_repository.rb
@@ -6,18 +6,19 @@
 # Here is how you would add these functions to an ActiveRecord:
 #
 #uncommented for production
- class Shop < ActiveRecord::Base
-   def self.store(session)
-     shop = Shop.new(domain: session.url, token: session.token)
-     shop.save!
-     shop.id
-   end
-
-   def retrieve(id)
-     shop = Shop.find(id)
-     ShopifyAPI::Session.new(shop.domain, shop.token)
-   end
+class Shop < ActiveRecord::Base
+ def self.store(session)
+   shop = Shop.new(domain: session.url, token: session.token)
+   shop.save!
+   shop.id
  end
+
+ def retrieve(id)
+   shop = Shop.find(id)
+   ShopifyAPI::Session.new(shop.domain, shop.token)
+ end
+end
 
 #commented for production
 #ShopifySessionRepository.storage = InMemorySessionStore
+ShopifySessionRepository.storage = Shop
